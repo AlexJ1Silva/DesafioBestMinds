92e87bf812a86e3d5e38f0883807a06de59bc3f2
Alex J. Silva <alexjsilva230@gmail.com>
2022-11-07 00:32:06 -0300

    implementação validação dos campos

--- Front.py
+++ Front.py
@@ -2,7 +2,7 @@
 import PySimpleGUI as sg
 import smtplib
 import email.message
-
+import re
 
 #empresa Oliveira Trade e foi solicitado pelo board da empresa que seja desenvolvido uma
 # forma de Sign in e Sign up de usuários.
@@ -12,7 +12,7 @@ import email.message
 def janela_sign_in(): #janela de Sign Up
     sg.theme('Reddit')
     layout = [
-        [sg.Text('Usuario:'), sg.Input(key='usuario', size=(21,1),)],
+        [sg.Text('Usuario:'), sg.Input(key='usuario', size=(21,1))],
         [sg.Text('Senha:'), sg.Input(key='senha', password_char='*', size=(21,1))],
         [sg.Text('Confirmar Senha'), sg.Input(key='repsenha', password_char='*', size=(21,1))],
         [sg.Text('Informe seu CPF'), sg.Input(key='cpf', size=(21, 1))],
@@ -28,7 +28,7 @@ def janela_sign_in(): #janela de Sign Up
 def janela_login(): #Janela de Sign In
     sg.theme('Reddit')
     layout = [
-        [sg.Text('Usuario:'), sg.Input(key='usuario', size=(20,1))],
+        [sg.Text('E-mail:'), sg.Input(key='email2', size=(20,1))],
         [sg.Text('Senha:'), sg.Input(key='senha', password_char='*', size=(21,1))],
         [sg.Checkbox('Salvar o Login', key='salvar')],
         [sg.Button('Entrar')],
@@ -36,21 +36,13 @@ def janela_login(): #Janela de Sign In
     return sg.Window('Login', layout=layout, finalize=True)
 
 
-def janela_sucesso():
+def janela_home():
     sg.theme('Reddit')
     layout = [
-        [sg.Text('Cadastro Efetuado com Sucesso!!')],
+        [sg.Text(f'Bem vindo {usuario}!!')],
+        [sg.Text('Verifique o seu E-mail para confirmar os dados cadastrados.')]
     ]
     return sg.Window('Sucesso', layout=layout, finalize=True)
-
-
-def janela_home(): #Janela de Sign In
-    sg.theme('Reddit')
-    layout = [
-        [sg.Text('Bem Vindo:')],
-
-    ]
-    return sg.Window('Home', layout=layout, finalize=True)
 
 
 janela1, janela2, janela3, janela4 = janela_sign_in(), None, None, None
@@ -80,7 +72,7 @@ def enviar_email():
     msg['Subject'] = "Desafio BestMInds - EveryMInd"  # assunto do email.
     msg['From'] = 'testdesafio@gmail.com'    #seu email - email criado - testdesafio@gmail.com
 
-    msg['To'] = 'ajs2mb@gmail.com'  #email destino #email que vai ser pego no input.
+    msg['To'] = f'{e_mail}'  #email destino #email que vai ser pego no input.
     password = 'pjsocmauivixnmoh' #Senha123
     msg.add_header('Content - Type', 'text/html')
     msg.set_payload(corpo_email )
@@ -91,6 +83,12 @@ def enviar_email():
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
 
+
+def solve(s):
+   pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+   if re.match(pat,s):
+      return True
+   return False
 
 
 
@@ -112,10 +110,16 @@ while True:
         tel = (entrada['telefone'])
         e_mail = (entrada['e_mail'])
 
-        if entrada['cpf'] == '' or entrada['endereco'] == '' or entrada['telefone'] == '' or entrada['e_mail'] == '':
+        if entrada['cpf'] == '' or entrada['endereco'] == '' or entrada['telefone'] == '' or entrada['e_mail'] == '': #validação campos vazios
             sg.popup('Preencha todos os campos.')
-        elif entrada['senha'] != entrada['repsenha']:
+        elif entrada['senha'] != entrada['repsenha']: #validação das senhas
             sg.popup("As senhas não se coincidem")
+        elif len(entrada['cpf']) != 11 or not entrada['cpf'].isdigit(): #validação do cpf
+            sg.popup('Campo CPF: Formato errado. Tente de novo (apenas numeros)')
+        elif len(entrada['telefone']) <10 or len(entrada['telefone']) > 11 or not entrada['telefone'].isdigit(): #validação telefone
+            sg.popup('Campo Telefone: Formato errado. Tente de novo (apenas numeros)')
+        elif not solve(entrada['e_mail']): #validação email
+            sg.popup('E-mail invalido')
 
 
 
@@ -134,10 +138,10 @@ while True:
 
     if window == janela2 and botoes == 'Entrar':
 
-        if entrada['usuario'] == usuario and entrada['senha'] == senha:
+        if entrada['email2'] == e_mail and entrada['senha'] == senha:
             sg.popup('Login Efetuado com Sucesso!')
 
-            janela3 = janela_sucesso()
+            janela3 = janela_home()
             janela2.hide()
         else:
 
